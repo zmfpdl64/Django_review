a38becb372e572137ee5d87cfd4a7ab064dc70c6
이우진 <zmfpdl64@naver.com>
2022-01-24 16:47:14 +0900

    대문 페이지와 자기소개 페이지 구현

--- woojin_web/blog/models.py
+++ woojin_web/blog/models.py
@@ -11,5 +11,5 @@ class Post(models.Model):
         return f'[{self.pk}][{self.title}]'   #Post객체가 호출되면 __str__함수가 실행되어 pk고유값과 title이 반환된다
                                             #[[self.pk][self.title]]이렇게 반환된다. f는 문자열 포맷팅이다.
                                         
-    def get_absolute_url(self):     #
+    def get_absolute_url(self):     #내부에서 정의되어있는 함수이다. 관리자 페이지에서 site에서 볼 수 있게 하는 함수
         return f'/blog/{self.pk}/'
