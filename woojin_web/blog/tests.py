fa39e8c71f64b40f4f46dc4eba9ec8dfb5e774ce
이우진 <zmfpdl64@naver.com>
2022-01-26 17:28:34 +0900

    상세페이지 테스트완료

--- woojin_web/blog/tests.py
+++ woojin_web/blog/tests.py
@@ -54,3 +54,34 @@ class TestView(TestCase):
         self.assertIn(post_002.title, main_area.text)
         #3.4 '아직 게시물이 없습니다.'라는 문구가 보이지 않는다.
         self.assertNotIn('아직 게시물이 없습니다.', main_area)
+    
+    def test_post_detail(self):
+        #1.1 포스트가 하나 있다.
+        post_001 = Post.objects.create(
+            title = "첫 번째 포스터입니다.",
+            content = "first",
+
+        )
+        self.assertEqual(Post.objects.count(), 1)
+        #1.2 그 포스트의 url은 '/blog/1/'이다.
+        self.assertEqual(post_001.get_absolute_url(), '/blog/1/')
+        
+        #2. 첫 번째 포스트의 상세 페이지 테스트
+        #2.1. 첫번째 프스트의 url로 접근하여 정상적인지 확인
+        response = self.client.get('/blog/1/')
+        self.assertEqual(response.status_code, 200)
+        #2.2 포스트의 네비게이션 바가 있는지 확인
+        soup = BeautifulSoup(response.content, 'html.parser')
+        navbar = soup.nav
+        self.assertIn('Blog', navbar.text)
+        self.assertIn('About me', navbar.text)
+        #2.3 웹 브라우저 탭 타이틀에 타이틀이 들어있나
+        self.assertIn(post_001.title, soup.title.text )
+        #2.4 첫 번째 포스트의 제목이 들어있나
+        main_area = soup.find('div', id='post-area')
+        self.assertIn(post_001.title, main_area.text)
+        #2.5 첫 번째 포스트에 작성자명이 들어있나
+
+        #2.6 첫 번째 포스트의 내용이 포스트영역에 있나
+        self.assertIn(post_001.content, main_area.text)
+
