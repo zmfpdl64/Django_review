658c1fd480cc0668031b0f71b7c86fe5b53bdf0d
이우진 <zmfpdl64@naver.com>
2022-01-27 12:41:38 +0900

    모달버튼 구현, 디자인 변경, 작성자 필드 생성

--- woojin_web/blog/models.py
+++ woojin_web/blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User #User db
 import os
 
 class Post(models.Model):
@@ -11,12 +12,14 @@ class Post(models.Model):
     head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True)   #urls에서 document의 기본경로를 세팅해놓았다.
     file_upload = models.FileField(upload_to='blog/files/%Y/%m/%d/', blank=True)
 
+    author = models.ForeignKey(User, on_delete=models.CASCADE) #user인 외래키와 연결해서 user가 삭제되면 같이 삭제된다.
+
     def __str__(self):
-        return f'[{self.pk}][{self.title}]'   #Post객체가 호출되면 __str__함수가 실행되어 pk고유값과 title이 반환된다
-                                            #[[self.pk][self.title]]이렇게 반환된다. f는 문자열 포맷팅이다.
+        return f'[{self.pk}][{self.title}] :: {self.author}'   #Post객체가 호출되면 __str__함수가 실행되어 pk고유값과 title이 반환된다
+                                            #[[self.pk][self.title]]이렇게 반환된다. f는 문자열 포맷팅이다. 관리자 페이지에서 자동으로 호출한다.
                                         
-    def get_absolute_url(self):     #내부에서 정의되어있는 함수이다. 관리자 페이지( admin ) 에서 site에서 볼 수 있게 하는 함수
-        return f'/blog/{self.pk}/'
+    def get_absolute_url(self):     #관리자 페이지( admin ) 에서 site에서 볼 수 있게 하는 함수
+        return f'/blog/{self.pk}/'  #이 함수가 구현되어 있으면 관리자 페이지에서 버튼이 자동으로 생긴다.
     
     def get_file_name(self):
         #print(type(self.file_upload.name))  #string 형식
--- woojin_web/blog/tests.py
+++ woojin_web/blog/tests.py
@@ -5,6 +5,26 @@ from .models import Post
 class TestView(TestCase):
     def setUp(self):
         self.client = Client()
+    
+    def navbar_test(self, soup):
+        navbar = soup.nav
+        self.assertIn('Blog', navbar.text)
+        self.assertIn('About me', navbar.text)
+
+        logo_btn = navbar.find('a', text='Woojin_Web') 
+        self.assertEqual(logo_btn.attrs['href'],'/')   #attrs: 속성값 모두 출력이다. 여기서 
+
+        home_btn = navbar.find('a', text='Home')
+        self.assertEqual(home_btn.attrs['href'], '/')
+
+        about_me_btn = navbar.find('a', text="About me")
+        self.assertEqual(about_me_btn.attrs['href'], '/about_me/')
+
+        blog_btn = navbar.find('a', text="Blog")
+        self.assertEqual(blog_btn.attrs['href'], '/blog/')
+        
+
+        
 
     def test_post_list(self):
         #1.1 포스트 목록 페이지를 가져온다.
@@ -19,11 +39,10 @@ class TestView(TestCase):
         #print(soup)
         self.assertEqual(soup.title.text, 'Blog')
         #1.4 내비게이션 바가 있다.
-        navbar = soup.find('nav') #1번
+        #navbar = soup.find('nav') #1번
         #navbar = soup.nav                  #2번
         #1.5 Blog, About me 문구가 있다.
-        self.assertIn('Blog', navbar.text)
-        self.assertIn('About me', navbar.text)
+        self.navbar_test(soup)
         
         # 2.1 메인 영역에 게시물이 하나도 없다면
         self.assertEqual(Post.objects.count(), 0)
@@ -55,33 +74,6 @@ class TestView(TestCase):
         #3.4 '아직 게시물이 없습니다.'라는 문구가 보이지 않는다.
         self.assertNotIn('아직 게시물이 없습니다.', main_area)
     
-    def test_post_detail(self):
-        #1.1 포스트가 하나 있다.
-        post_001 = Post.objects.create(
-            title = "첫 번째 포스터입니다.",
-            content = "first",
-
-        )
-        self.assertEqual(Post.objects.count(), 1)
-        #1.2 그 포스트의 url은 '/blog/1/'이다.
-        self.assertEqual(post_001.get_absolute_url(), '/blog/1/')
-        
-        #2. 첫 번째 포스트의 상세 페이지 테스트
-        #2.1. 첫번째 프스트의 url로 접근하여 정상적인지 확인
-        response = self.client.get('/blog/1/')
-        self.assertEqual(response.status_code, 200)
-        #2.2 포스트의 네비게이션 바가 있는지 확인
-        soup = BeautifulSoup(response.content, 'html.parser')
-        navbar = soup.nav
-        self.assertIn('Blog', navbar.text)
-        self.assertIn('About me', navbar.text)
-        #2.3 웹 브라우저 탭 타이틀에 타이틀이 들어있나
-        self.assertIn(post_001.title, soup.title.text )
-        #2.4 첫 번째 포스트의 제목이 들어있나
-        main_area = soup.find('div', id='post-area')
-        self.assertIn(post_001.title, main_area.text)
-        #2.5 첫 번째 포스트에 작성자명이 들어있나
-
-        #2.6 첫 번째 포스트의 내용이 포스트영역에 있나
-        self.assertIn(post_001.content, main_area.text)
+    
+    
 
